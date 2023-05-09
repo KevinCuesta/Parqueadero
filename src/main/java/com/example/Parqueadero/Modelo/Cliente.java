0778bb6e4d707fd9572c6f7346b524b114b5c83c
Kevin Cuesta <CRACKESAN@GMAIL.COM>
2023-05-09 15:36:00 -0500

    Creacion Clientes
    Se creo el modelo cliente  con sus respectivos get y set
    
    Co-Authored-By: Will980410 <131024524+Will980410@users.noreply.github.com>

---
+++ src/main/java/com/example/Parqueadero/Modelo/Cliente.java
@@ -0,0 +1,62 @@
+package com.example.Parqueadero.Modelo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    @Column(name = "cedula")
+    private String cedula;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    public Cliente() {}
+
+    public Cliente(String cedula, String nombre, String apellido, String telefono) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+    
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
